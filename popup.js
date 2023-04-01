fcf9fd708b154e3fc23bfacf7b2923daff2c1167
maxzhang <373884384@qq.com>
2023-04-01 20:55:49 +0800

    移除日志

--- popup.js
+++ popup.js
@@ -3,6 +3,6 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 	$("#title").val(tabs[0].title);
 	$("#icon").val(tabs[0].favIconUrl);
 	$("#icon_view").attr('src',tabs[0].favIconUrl);
-	console.log(tabs[0])
+	// console.log(tabs[0])
 	// $("#description").val();
 });
